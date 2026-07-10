eb4eea1aee2ffad82125c2c3de90368c257adf4e
Connor Bryant <connorjamesbryant7@gmail.com>
2026-07-10 18:02:51 -0400

    Randomness update

--- js/app.js
+++ js/app.js
@@ -1,23 +1,17 @@
 jQuery(function($){
 
-    var startBtn = $("#startBtn");
-    var gameList = $("#gameList");
-
-    // Generate a random number from 1 to 10 (gameList)
+    // Generate a random number from 1 to 10
     const minAmt = 1;
     const maxAmt = 10;
-    const randomUlNum =  Math.floor(Math.random() * (maxAmt - minAmt + 1)) + minAmt;
-
-    // gameList list elements and their random index
-    var gameLi = $("#gameList > li");
-    var gameLiCount = gameLi.length;
-    var randomLiIndex = Math.floor(Math.random() * gameLiCount);
-    var randomLiRender = gameLi.eq(randomLiIndex);
+    const randomNum =  Math.floor(Math.random() * (maxAmt - minAmt + 1)) + minAmt;
 
     // For loop to append the list elements
-    for (let i = 1; i <= randomUlNum; i++){
-        $(gameLi).append(`<li>Bullet</li>`);
-        randomLiRender.append(`<div class='worm'></div>`);
+    for (let i = 1; i <= randomNum; i++){
+        $("#gameList").append(`<li>Item #${i}</li>`);
+        
+        $("#gameList li").each(function(i){
+            $(this).eq(Math.random(i)).append(`<div class="worm"></div>`);
+        });
     }
 });
 
