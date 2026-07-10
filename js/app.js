96ffca31a2d37200a700e9c88d530711ecd76dd8
Connor Bryant <connorjamesbryant7@gmail.com>
2026-07-10 17:12:39 -0400

    JS update

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@ jQuery(function($){
 
     var startBtn = $("#startBtn");
     var gameList = $("#gameList");
+    var gameListItem = $("#gameList li");
 
     // Generate a random number from 1 to 10
     const minAmt = 1;
@@ -10,7 +11,8 @@ jQuery(function($){
 
     // For loop to append the list elements
     for (let i = 1; i <= randomNum; i++){
-        $(gameList).append(`<li><div class='worm'></div></li>`);
+        $(gameList).append(`<li>Bullet</li>`);
+        $(gameListItem).eq(randomNum).append("<div class='worm'></div>");
     }
 });
 
