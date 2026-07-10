95e14bc9956629a35f8b0cffb86d59b6176ee42e
Connor Bryant <connorjamesbryant7@gmail.com>
2026-07-10 17:53:38 -0400

    Working on random generation

--- js/app.js
+++ js/app.js
@@ -2,18 +2,22 @@ jQuery(function($){
 
     var startBtn = $("#startBtn");
     var gameList = $("#gameList");
-    var gameListItem = $("#gameList > li");
 
-    // Generate a random number from 1 to 10
+    // Generate a random number from 1 to 10 (gameList)
     const minAmt = 1;
     const maxAmt = 10;
-    const randomNum =  Math.floor(Math.random() * (maxAmt - minAmt + 1)) + minAmt;
+    const randomUlNum =  Math.floor(Math.random() * (maxAmt - minAmt + 1)) + minAmt;
+
+    // gameList list elements and their random index
+    var gameLi = $("#gameList > li");
+    var gameLiCount = gameLi.length;
+    var randomLiIndex = Math.floor(Math.random() * gameLiCount);
+    var randomLiRender = gameLi.eq(randomLiIndex);
 
     // For loop to append the list elements
-    for (let i = 1; i <= randomNum; i++){
-        $(gameList).append(`<li>Bullet</li>`);
-        //$(gameList).next().eq(randomNum).append(`<div>Worm</div>`);
-        $(gameListItem).append(`<div>Worm</div>`);
+    for (let i = 1; i <= randomUlNum; i++){
+        $(gameLi).append(`<li>Bullet</li>`);
+        randomLiRender.append(`<div class='worm'></div>`);
     }
 });
 
