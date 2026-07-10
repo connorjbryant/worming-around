85ca15d95988c6079ae8cc287bb3cedefb67046b
Connor Bryant <connorjamesbryant7@gmail.com>
2026-07-10 15:13:27 -0400

    Thinking through the game

--- js/app.js
+++ js/app.js
@@ -1,3 +1,20 @@
 jQuery(function($){
 
 });
+
+/**
+ * Ideas
+ * 
+ * Movement
+ * 
+ * $(document).on("keydown", function(e){
+        switch (e.key){
+            case "ArrowLeft":
+                //function maybe moveLeft();
+                break;
+            case "ArrowRight":
+                // maybe moveRight();
+                break;
+        }
+    })
+ */
